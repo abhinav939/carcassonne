c3a6f447f3811d54ee065c2f9589426dd12a9e55
Dexter Lakin <dexterlakin@hotmail.com>
2021-07-16 21:29:51 +0100

    remove unused test files

--- src/road.test.ts
+++
@@ -1,8 +0,0 @@
-
-// const road = require('./road');
-
-// describe('road test', () => {
-//     test('adds 1 + 2 to equal 3', () => {
-//         expect(road(1, 2)).toBe(3);
-//     });
-// });
--- src/tilestack.test.ts
+++
@@ -1,8 +0,0 @@
-// Arrange
-// Select base tile set (no expansion packs)
-
-// Act
-// When game starts
-
-// Assert
-// len(tilestack) should == 72
